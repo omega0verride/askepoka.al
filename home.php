2a496fcae1a564fce222cb11e22f8847911a61b4
omega0verride <om3ga0verride@gmail.com>
2022-06-04 22:48:17 +0200

    created a card structure for each post

--- home.php
+++ home.php
@@ -5,6 +5,39 @@
     <title>Welcome</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="assets/stylesheet.css" />
+
+    <style>
+        .card-table{
+            /* border: 1px solid white; */
+        }
+        .card {
+            padding-left: 3%;
+            width: 94%;
+            text-align: center;
+        }
+
+        .card-title {
+            text-align: center;
+        }
+
+        .card-content {
+            text-align: left;
+            padding: 5px;
+        }
+
+        .card-controls {
+            text-align: right;
+        }
+
+        .votes {
+            float: left;
+            border: 1px solid red;
+            background-color: red;
+            text-align: center;
+            width: 100px;
+            height: 100%;
+        }
+    </style>
 </head>
 
 <body>
@@ -25,6 +58,27 @@
         include("templates/login_prompt.html");
     }
     ?>
+
+    <div class="card">
+        <table class="card-table">
+            <tr class="card-table">
+                <td rowspan="2" class="card-table">
+                    <div class="votes">
+                        <button>Up</button>
+                        <p id="cnt">0</p>
+                        <button>Down</button>
+                    </div>
+                </td>
+                <td class="card-table card-title">What is Lorem Ipsum?</td>
+            </tr>
+            <tr class="card-table">
+                <td class="card-table card-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</td>
+            </tr>
+            <tr class="card-table">
+                <td colspan="2" class="card-table card-controls">Comments, report, other options</td>
+            </tr>
+        </table>
+    </div>
 </body>
 
 </html>

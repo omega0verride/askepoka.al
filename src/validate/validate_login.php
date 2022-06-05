6a4d1818f38181c728a49adbf1e9adcec9d1621a
omega0verride <om3ga0verride@gmail.com>
2022-06-05 22:42:47 +0200

    fixed login redirect url

--- src/validate/validate_login.php
+++ src/validate/validate_login.php
@@ -22,7 +22,7 @@ if ($password === null || $password === "") {
         $stmt = $conn->prepare($sql);
         $stmt->execute(array($username));
         $row = $stmt->fetch();
-
+        
         if ($row == null || $row["password"] !== md5($password)) {
             echo "Password or username did not match!";
             $_SESSION["loginError"] = "Password or username did not match!";
@@ -35,7 +35,7 @@ if ($password === null || $password === "") {
             setcookie("username", $username, time() + (86400 * 30), "/"); // save credentials for 30 days unless the user logs out
             setcookie("hashedPass", md5($password), time() + (86400 * 30), "/"); // save credentials for 30 days unless the user logs out
         }
-        if (isset($_GET["redirect"])) {
+        if (isset($_GET["redirect"])&&$_GET["redirect"]!=null) {
             header('Location:'.$_GET["redirect"]);
         } else {
             header('Location:../../account.php');
